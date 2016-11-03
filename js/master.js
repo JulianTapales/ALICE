4abf98fc08fff45cffb8b9675ace4ccd220ad045
Julian <julian.tapales@gmail.com>
2016-11-03 23:11:41 +0000

    Played around with random distribution

--- js/master.js
+++ js/master.js
@@ -1,16 +1,43 @@
 var canvas = $("#main")[0]
 var ctx = canvas.getContext("2d");
 
-function random(){
-  return Math.random()*1000
+ctx.lineWidth = 0.5;
+
+function Point(x, y){
+  var o = this;
+  o.x = x;
+  o.y = y;
+  return this
 }
 
-for(i=1; i<=40; i++){
+function random(){
+  var r = Math.random()
+  function ball(){ return 0.25*(Math.random()+Math.random()+Math.random()+Math.random()) }
+  function diagonal() {return (Math.random()+Math.random())}
+  function normal() { return Math.random() }
+  function bigDiagonal(){return  Math.max(Math.random(), Math.random())}
+
+  return (0.2*ball()+diagonal()+10*normal())/12
+}
+
+function randomPoint(){
+  return new Point(random()*1000, random()*1000)
+}
+
+var currentLines = []
+
+for(i=1; i<=4000; i++){
   // ctx.fillStyle = "green";
   // ctx.fillRect(random(), random(), random(), random());
 
+  var start = randomPoint()
+  var end = randomPoint()
+
+  currentLines.push({start: start, end: end})
+
+  //ctx.strokeStyle = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
   ctx.beginPath();
-  ctx.moveTo(random(),random());
-  ctx.lineTo(random(), random());
+  ctx.moveTo(start.x, start.y);
+  ctx.lineTo(end.x, end.y);
   ctx.stroke();
 }
